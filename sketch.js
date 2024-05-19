5d7ceefeb2f2c1c86283e3a20f0874c92bd1cc39
leonflax <leonflaxb@gmail.com>
2024-05-19 17:06:37 -0300

    Crecimiento de figuras

--- sketch.js
+++ sketch.js
@@ -3,18 +3,15 @@ let texturaFigura;
 let figuras = [];
 let indiceFiguras = 0;
 let maxFiguras = 30; // cantidad max de figuras
-let cantFiguras = 0;
-let bandera = false; //detecta mouse
 let figuraActual = null; // figura actual
 let imgPaleta;
 let paleta;
 let radInicial = 30;
-let radMax = 200;
+let radMax = 350;
 
 function setup() {
   createCanvas(windowHeight / 1.5, windowHeight, WEBGL);
   paleta = new Paleta(imgPaleta);
-  Figura.estado = "inicial"; // Estado inicial por defecto
 }
 
 function draw() {
@@ -22,22 +19,29 @@ function draw() {
   noStroke();
 
   push();
-  //blendMode(MULTIPLY); //modo de fusion lienzo y figuras
   tint(255, 100); // color de lienzo
-  image(textura, -width, -height, width * 2, height * 2); //imagen lienzo
+  image(textura, -width, -height, width * 2, height * 2); // imagen lienzo
   pop();
 
+  // Mostrar todas las figuras en el array
   for (let i = 0; i < figuras.length; i++) {
-    //leer array
-    figuras[i].show(); //mostrar las figuras
+    figuras[i].show();
   }
+
+  // Mostrar la figura actual en proceso de crecimiento
   if (figuraActual) {
-    figuraActual.show(); // mostrar figura actual en proceso
+    figuraActual.show();
+    if (figuraActual.radio >= radMax) {
+      fijarFiguraActual();
+    }
   }
+}
 
-  push();
-
-  pop();
+function fijarFiguraActual() {
+  figuraActual.estado = 'fijo';
+  figuras.push(figuraActual);
+  figuraActual = null;
+  indiceFiguras++;
 }
 
 rightClick(); // Desactiva el click derecho
